b8bb255fade1d952d2b2bcc99963501e209a4473
abdiel-249 <abdielorellana3@gmail.com>
2018-08-01 15:02:46 -0400

    ejercicios de recursividad 1- 3*

---
+++ recursividad/Ejercicios/Ejercicios.java
@@ -0,0 +1,288 @@
+class recursividad{
+    
+    public int potencia(int a, int b){
+        if(b==1)
+            return a;
+        else 
+            return a*potencia(a, b-1);
+    }
+    // num es primo
+    public boolean esPrimo(int num){
+        if(divisores(num,1)==1) return true;
+        return false;
+    }
+    private int divisores(int num, int div){
+        int resp=0;
+        if(div <= (Math.sqrt(num))){
+            if(num%div == 0)
+                resp+=1;
+            resp += divisores(num, div+1);
+        }
+        return resp;
+    }
+
+    //cantidad de digitos
+    public int cantidadDigitos(int num){
+        int cant=0;
+        if(num>0){
+            cant+=1+cantidadDigitos(num/10);
+        }
+        return cant;
+    }
+
+    //digitos de un numero n son primos
+    boolean allPar(int num){
+        boolean resp= true;
+        if(num>0){
+            if(resp && ((num%10)%2)==0){
+                resp=true && allPar(num/10);
+            }else   
+                resp=false;
+        }
+        return resp;
+    }
+
+    // 15.- Divisible entre 11
+    boolean divOnce(int num){
+        boolean resp = false;
+        if((sumDic(num, true, 0))%11 == 0){
+            resp=true;
+        }
+        return resp;
+    }//2648967 -> 2-6+4-8+9-6+7
+    private int sumDic(int num, boolean sw, int sum){
+        int resp=0;
+        if(num>0){
+            if(sw){
+                resp = sumDic(num/10, false, resp);
+                resp += sum + num%10;
+            }else{
+                resp = sumDic(num/10, true, resp);
+                resp += sum - num%10;
+            }
+        }
+        return resp;
+    }
+
+    // 17.- cadenas
+    // a) contar caracter
+    int cantChar(String cad, char c){
+        int resp = cantChar(cad, c, 0, 0);
+        return resp;
+    }
+    private int cantChar(String cad, char c,int pos, int cant){
+        int resp = 0;
+        if(pos<cad.length()){
+            if(cad.charAt(pos)== c){
+                resp+=1+cantChar(cad, c, pos+1, resp);
+            }else
+                resp+=cantChar(cad, c, pos+1, cant);
+        }
+        return resp;
+    }
+    // b) invertir cadena
+    String invertit(String cad){
+        String resp=invertir(cad, 0);
+        return resp;
+    }
+    private String invertir(String cad, int pos){
+        String resp="";
+        if(pos<cad.length()){
+            resp=invertir(cad, pos+1)+cad.charAt(pos);
+        }
+        return resp;
+    }
+    // c) contar vocales
+    int cantVocales(String cad){
+        int resp=cantVocales(cad, 0);
+        return resp;
+    }
+    private int cantVocales(String cad, int pos){
+        int resp=0;
+        if(pos<cad.length()){
+            char aux = cad.charAt(pos);
+            if(aux=='a' || aux=='e' || aux=='i' || aux=='o' || aux=='u'){
+                resp=1;
+            }
+            resp+=cantVocales(cad, pos+1);
+        }
+        return resp;
+    }
+
+    // 20) palabra palindromo
+    boolean palindromo(String cad){
+        boolean resp=palindromo(cad, 0, true);
+        return resp;
+    }
+    private boolean palindromo(String cad, int pos, boolean sw){
+        boolean resp = true;
+        if(pos<(cad.length()/2)){
+            char aux=cad.charAt(pos);
+            if(sw && aux==cad.charAt(cad.length()-pos-1))
+                resp = true;
+            else
+                resp =false;
+            resp = resp && palindromo(cad, pos+1, resp);
+        }
+        return resp;
+    }
+
+    // 21) suma n numeros mult 4
+    int calcuSumCuatro(int n){
+        int resp=calcuSumCuatro(n, 0);
+        return resp;
+    }
+    private int calcuSumCuatro(int num, int cont){
+        int resp=0;
+        if(num>0){
+            resp+=cont+calcuSumCuatro(num-1, cont+4);
+        }
+        return resp;
+    }
+    //                                   012345 - 012
+    // 28) a) Palabra X prefijo de Y     lordem - dem
+    boolean esPrefijo(String cad, String pref){
+        return esPrefijo(cad, pref, pref.length()-1, true);
+    }
+    private boolean esPrefijo(String cad, String pref, int pos, boolean sw){
+        boolean resp=true;
+        if(pos>0 && sw){
+            if(cad.charAt(pos) == pref.charAt(pos))
+                resp = resp && esPrefijo(cad, pref, pos-1, resp);
+            else
+                resp = false;
+        }
+        return resp;
+    }
+    // 28) b) Palabra X sufijo de Y     lordem - dem = true
+    boolean esSufijo(String cad, String pref){
+        return esSufijo(cad, pref, (cad.length()-pref.length()), true);
+    }
+    private boolean esSufijo(String cad, String pref, int pos, boolean sw){
+        boolean resp=true;
+        if(pos<cad.length() && sw){
+            if(cad.charAt(pos) == pref.charAt(pos-(pref.length()))){
+                resp = resp && esSufijo(cad, pref, pos+1, resp);
+            }else
+                resp = false;
+        }
+        return resp;
+    }
+
+    /*                  EXTRA 
+        hacer un proceso recursivo que debuelva/imprima la siguiente secuencia de cadenas,
+                                                             dado un numero N 1<= n <= 100
+    *      1/2     1/3     1/4     1/5     1/8     1/9     1/16    1/17    1/32 ...
+cuando N=   1       2       3       4       5       6       7       8       9   ...
+            impar   par     impar   par     impar   par     impar   par   impar ....
+    *   tome en cuenta que el unico dato de entrada es un numero entero positivo, suerte :v
+    * NOTA:  (la respuesta se espera que sea una cadena (String) y es opcional imprimirlo)  
+    */
+    String secuencia(int n){
+        return theFuckSecuen(n, 1, "", 2);
+    }
+    private String theFuckSecuen(int n, int ini, String cad, int numPar){
+        String resp="";
+        if(ini<=n){
+            if(ini%2 == 1){
+                resp = "1/"+numPar;
+                resp += "\t"+theFuckSecuen(n, ini+1, resp, numPar);
+            }else{
+                resp = "1/"+(numPar+1);
+                resp += "\t"+theFuckSecuen(n, ini+1, resp, numPar*2);
+            }
+        }
+        return resp;
+    }
+
+    /// chinitos de nivel n
+    String chinitos(int n){
+        if(n==1)
+            return "(-_-)";
+        else
+            return "(-"+chinitos(n-1)+"-)";
+    }
+
+
+    // 31) el cambio perfecto
+    String cambiar(double monto){
+        String resp = "";
+        Dou d = new Dou(monto);
+        double[] billetes = {100, 50, 20, 10, 5, 2};
+        double[] monedas = {0.50, 0.25, 0.1, 0.05, 0.01};
+        resp = "\t *** BILLETES ***\n"+contar(billetes, 0, d, "Billetes")+
+               "\t *** MONEDAS ***\n" +contar(monedas, 0, d, "Monedas");
+        return resp;
+    }
+    String contar(double[] cambiar, int pos, Dou monto, String tipoCambio){
+        String resp="";
+        if(pos<cambiar.length){
+            int cant = cantUnidades(monto.getNum(), cambiar[pos]);
+            if(cant>0){
+                newMonto(monto, cambiar[pos]);
+                resp = cant +"\t"+tipoCambio+" de "+cambiar[pos]+"\n"+contar(cambiar, pos+1, monto, tipoCambio);
+            }else{
+                resp = contar(cambiar, pos+1, monto, tipoCambio);
+            }
+        }
+        return resp;
+    }
+    private double newMonto(Dou monto, double cambio){
+        double resp = 0;
+        if(monto.getNum()>=cambio){
+            monto.rest(cambio);
+            resp = monto.getNum() - newMonto(monto, cambio);   
+        }
+        return resp;
+    }
+    private int cantUnidades(double monto, double cambio){
+        int resp=0;
+        if(monto>=cambio){
+            resp =1+cantUnidades((monto-cambio), cambio);
+        }
+        return resp;
+    }
+    private class Dou{
+        private double num;
+        public Dou(double n){
+            this.num = n;
+        }
+        public void rest(double val){
+            this.num-= val;
+        }
+        public double getNum(){
+            return num;
+        }
+    }
+
+
+    
+    // 32) eliminar segudas ocurencias de un numero entero de Izq a Der
+    String elimOcurrencias(int num){
+        String cad = num+"";
+        return (elim(cad, cad.length()-1, ""));
+    }//                     2656342 =>  56342
+    private String elim(String cad, int pos, String vistos){
+        String resp="";
+        if(pos>=0){                             //  2656342 =>  56342
+            char aux = cad.charAt(pos);         //  0123456     01234
+            if(! yaEsta(aux, vistos, 0)){
+                vistos += aux+"";
+            }
+            resp = elim(cad, pos-1, vistos)+aux+"";
+        }
+        return resp;
+    }
+    private boolean yaEsta(char a, String cad, int pos){
+        boolean resp = false;
+        if(pos<cad.length()){
+            if(cad.charAt(pos)== a)
+                resp = true;
+            else
+                resp = yaEsta(a, cad, pos+1);
+        }
+        return resp;
+    }
+
+
+}
---
+++ recursividad/Ejercicios/mainRecu.java
@@ -0,0 +1,64 @@
+public class mainRecu{
+    
+    public static void main(String[]args){
+        recursividad r;
+        r = new recursividad();
+
+        //sms("ejercisio 1 potencia de  = "+r.potencia(4, 3));
+        
+        //sms("el numero 15 es primo? => "+r.esPrimo(15));
+        //sms("el numero 7 es primo? => "+r.esPrimo(7));
+        //sms("el numero 23 es primo? => "+r.esPrimo(23));
+        
+        //sms("el numero 12345 tiene : "+r.cantidadDigitos(12345)+" digitos");
+        //sms("el numero 123456789 tiene : "+r.cantidadDigitos(123456789)+" digitos");
+        
+        //sms("todos los numeros de 2468 son pares? "+r.allPar(2468));
+        //sms("todos los numeros de 2456 son pares? "+r.allPar(2456));
+        //sms("todos los numeros de 52426 son pares? "+r.allPar(52426));
+        //sms("todos los numeros de 28642086 son pares? "+r.allPar(28642086));
+
+        //sms("suma/resta de digitos del num 2341675 es: "+r.sumDic(2341675, true, 0)+" es div entre 11"+r.divOnce(2341675));
+        //sms("suma/resta de digitos del num 2648967 es: "+r.sumDic(2648967, true, 0)+" es div entre 11"+r.divOnce(2648967));
+        
+        //sms("la canticad de 'e' en elementos de programacion es "+r.cantChar("elementos de programacion", 'e'));
+        //sms("la canticad de 'e' en 'adele' es "+r.cantChar("adele", 'e'));
+        
+        //sms("la cadena 'hola mundo' invertida sera '"+r.invertit("hola mundo")+"'");
+    
+        //sms("la cadena 'elementos de programacion' tiene "+r.cantVocales("elementos de programacion")+" vocales");
+        //sms("la cadena 'holamundo' tiene "+r.cantVocales("holamundo")+" vocales");
+        //sms("la cadena 'sistemas informaticos universitarios' tiene "+r.cantVocales("sistemas informaticos universitarios")+" vocales");
+        //sms("la cadena 'eucalipto' tiene "+r.cantVocales("sistemas informaticos universitarios")+" vocales");
+   
+        //sms("la cadena 'omo' es palintromo? "+r.palindromo("omo"));
+        //sms("la cadena 'hola' es palintromo? "+r.palindromo("hola"));
+        //sms("la cadena 'ya no on ay' es palintromo? "+r.palindromo("ya no on ay"));   
+
+        //sms("la suma de los primero 4 num mult de 4 es "+r.calcuSumCuatro(4));
+
+        //sms("lord es prefijo de 'lordem' :"+r.esPrefijo("lordem", "lord"));
+        //sms("'gen' es prefijo de 'gamdalf' :"+r.esPrefijo("gamdalf", "gen"));
+
+        //sms("'dem' es sufijo de lordem : "+r.esSufijo("lordem", "dem"));
+        //sms("'eta' es sufijo de 'soquete' : "+r.esSufijo("soquete", "eta"));
+
+        //sms("theFuckSecuency of 3 is: "+r.secuencia(3));
+        //sms("theFuckSecuency of 8 is: \n\t"+r.secuencia(8));
+        //sms("theFuckSecuency of 20 is: \n\t"+r.secuencia(20));
+
+        //sms("chinitos de nivel 3 = "+r.chinitos(3));
+        //sms("chinitos de nivel 1 = "+r.chinitos(1));
+        //sms("chinitos de nivel 5 = "+r.chinitos(5));
+
+        //sms("el cambio perfacto para 54 pesos es :\n"+r.cambiar(54.0));
+        //sms("el cambio perfacto para 62.56 pesos es :\n"+r.cambiar(62.56));
+        //sms("el cambio perfacto para 125.36 pesos es :\n"+r.cambiar(125.36));
+
+        sms("al eliminar las segundas ocurencias del numero 2656342 =>  56342 -> "+r.elimOcurrencias(2656342));
+    }
+
+    private static void sms (String cad){
+        System.out.println(cad);
+    } 
+}
